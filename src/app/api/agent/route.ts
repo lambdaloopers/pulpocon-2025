7ff8f20c8dc51478e094c08446349caf235f4bc8
Adrià Batlle <sudobat@gmail.com>
2025-09-18 16:50:42 +0200

    Add @modelcontextprotocol/sdk dependency and enhance agent API integration
    Updated package.json and package-lock.json to include the @modelcontextprotocol/sdk dependency. Enhanced the agent API integration in route.ts by implementing a new MCP client for database interactions, allowing for improved response handling based on PostgreSQL database structure. Adjusted the system prompt for the agent to focus on database queries and added logic to retrieve available tools from the MCP client.

--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -1,15 +1,41 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { convertToModelMessages, streamText, UIMessage } from 'ai';
+import { convertToModelMessages, streamText, UIMessage, stepCountIs } from 'ai';
 import { openai } from '@ai-sdk/openai';
+import { experimental_createMCPClient as createMCPClient } from 'ai';
+import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
 
 export async function POST(req: NextRequest) {
   try {
     const { messages }: { messages: UIMessage[] } = await req.json();
 
+    const dsn = process.env.DATABASE_URL || '';
+    const mcpClient = await createMCPClient({
+      transport: new StdioClientTransport({
+        command: 'npx',
+        args: [
+          '-y',
+          '@bytebase/dbhub',
+          '--dsn',
+          dsn
+        ],
+      }),
+    });
+
+    const tools = await mcpClient.tools();
+
     const result = streamText({
       model: openai('gpt-4.1'),
-      system: 'Eres un agente encargado de defender los beneficios de la PulpoCon 2025, un evento gastrotech increible que se celebra en la ciudad de Vigo alrededor del desarrollo de software.',
+      system: `
+      Eres un agente encargado de responder preguntas sobre la base de datos Postgresql que tienes disponible.
+
+      IMPORTANTE:
+      - Siempre mira que tablas tienes disponibles antes de responder.
+      - Siempre mira que columnas tiene cada tabla antes de responder.
+      - Siempre mira que relaciones tiene cada tabla antes de responder.
+      `,
       messages: convertToModelMessages(messages),
+      tools: tools,
+      stopWhen: stepCountIs(10),
     });
 
     return result.toUIMessageStreamResponse()
